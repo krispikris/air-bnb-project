d854fce0415f5cfc723163aeb9796ee3e3698e98
Kristopher Han <kris@kristopherhan.com>
2022-10-01 20:59:02 -0700

    testing getAllSpots

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -1,20 +1,53 @@
 const express = require('express');
 const router = express.Router();
 
-const { User, Spot, SpotImage, Review, ReviewImage, Booking } = require('../../db/models');
+const { User, Spot, SpotImage, Review, ReviewImage, Booking, sequelize } = require('../../db/models');
 const { requireAuth } = require('../../utils/auth');
-const sequelize = require('sequelize');
 const { Op } = require('sequelize');
 
 // #06: GET ALL SPOTS
-router.get('/', async (req, res) => {
-    let { page, size } = req.query;
+// router.get('/', async (req, res) => {
+//     let { page, size } = req.query;
+//     page =
+//          page === undefined ? 1  : page < 0 ? 1 : page > 10 ? 10 : parseInt(page);
 
-    page =
-         page === undefined ? 1 : page < 0 ? 1 : page > 10 ? 10 :
+//     size =
+//          size === undefined ? 20 : size < 0 ? 1 : size > 20 ? 20 : parseInt(size);
 
-    page = parseInt(page);
-    size = parseInt(size);
+//     let pagination = {};
+//     if (page >= 1 && size >= 1) {
+//         pagination.limit = size;
+//         pagination.offset = size * (page - 1);
+//     }
 
+//     const spots = await Spot.findAll( { ...pagination } );
+//     for (let spot of spots) {
+//         spot = spot.toJSON();
 
-})
+//         const ratings = await Review.findAll({
+//             raw: true,
+//             where: { spotId: spot.id },
+//             attributes: [
+//                 [ sequelize.fn('AVG', sequelize.col('stars')), 'avgRating' ]
+//             ]
+//         });
+
+//         const imageURL = await SpotImage.findOne({
+//             where: {
+//                 spotId: spot.id,
+//                 preview: true
+//             },
+//             attributes: ['url'], raw: true
+//         });
+
+//         spot.avgRating = Number(ratings[0].avgRating).toFixed(2)
+//         spot.previewImage = imageURL.url;
+
+//         let spotsArr = [];
+//         spotsArr.push(spot);
+//     };
+
+//     res.status(200).json({ 'Spots': spotsArr, page, size });
+// });
+
+module.exports = router;
