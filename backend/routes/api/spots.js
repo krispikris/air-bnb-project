8e3134cac5a6e034962709f0222b4c43aaa64895
Kristopher Han <kris@kristopherhan.com>
2022-10-02 02:02:15 -0700

    halfway point for spots route

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -6,10 +6,115 @@ const { handleValidationErrors } = require('../../utils/validation');
 const { requireAuth } = require('../../utils/auth');
 const { Op } = require('sequelize');
 
-// #07: GET ALL SPOTS OWNED BY THE CURRENT USER
-router.get('/current', async (req, res) => {
+// #09 | #10: CREATE AN IMAGE FOR A SPOT
+router.post('/:spotId/images', requireAuth, async (req, res) => {
+    const { url } = req.body;
+    const spot = await Spot.findByPk(req.params.spotId);
+
+    if (!spot) {
+        res.status(404);
+        res.json({
+            message: "Spot couldn't be found",
+            statusCode: 404
+        });
+    };
+
+    const spotImage = await SpotImage.create({
+        spotId: req.params.spotId,
+        url: url,
+        preview: true
+    });
+
+    return res.json({
+        id: spotImage.id,
+        url: spotImage.url,
+        preview: spotImage.preview
+    });
+});
+
+// #08: CREATE A SPOT
+router.post('/', requireAuth, async (req, res) => {
+    const user = req.user;
+    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+
+    try {
+        const newSpot = await Spot.create({
+            ownerId: user.id,
+            address,
+            city,
+            state,
+            country,
+            lat,
+            lng,
+            name,
+            description,
+            price
+        });
+
+        return res.json(newSpot);
+    }
+
+    catch {
+        res.status(400);
+        res.json({
+            message: 'Validation Error',
+            statusCode: 400,
+            errors: {
+                'address': 'Street address is required',
+                'city': 'City is required',
+                'state': 'State is required',
+                'country': 'Country is required',
+                'lat': 'Latitude is not valid',
+                'lng': 'Longitude is not valid',
+                'name': 'Name must be less than 50 characters',
+                'description': 'Description is required',
+                'price': 'Price per day is required'
+              }
+        });
+    };
+});
+
+// #12: GET DETAILS OF A SPOT BY ID
+router.get('/:spotId', requireAuth, async (req, res) => {
 
 })
+
+// #11: GET ALL SPOTS OWNED BY THE CURRENT USER
+router.get('/current', requireAuth, async (req, res) => {
+    let ownerId = req.user.id;
+
+    const spots = await Spot.findAll({
+        // raw:     true,
+        // include: { model: SpotImage, where: { preview: true } },
+        where:   { ownerId : ownerId }
+    });
+
+    const result = [];
+    for (let spot of spots) {
+        spot = spot.toJSON()
+
+        const ratings = await Review.findAll({
+            where: { spotId: spot.id },
+            attributes: [[ sequelize.fn('AVG', sequelize.col('stars')), 'avgRating' ]]
+        });
+
+        const imageURL = await SpotImage.findOne({
+            where: {
+                spotId: spot.id,
+                preview: true
+            },
+            attributes: ['url']
+        });
+
+        result.push(spot);
+        spot.avgRating = Number(ratings[0].toJSON().avgRating);
+        spot.previewImage = imageURL.url;
+
+        // delete spot.SpotImages;
+    };
+
+    res.json({ Spots: result });
+});
 
 // #06: GET ALL SPOTS | WORKS!
 // Look into the avgRating syntax, not giving the math avg rn
@@ -29,17 +134,14 @@ router.get('/', async (req, res) => {
     }
 
     const spots = await Spot.findAll( { ...pagination } );
-    let spotsArr = [];
 
+    let result = [];
     for (let spot of spots) {
         spot = spot.toJSON();
 
         const ratings = await Review.findAll({
-            raw: true,
             where: { spotId: spot.id },
-            attributes: [
-                [ sequelize.fn('AVG', sequelize.col('stars')), 'avgRating' ]
-            ]
+            attributes: [[ sequelize.fn('AVG', sequelize.col('stars')), 'avgRating' ]]
         });
 
         const imageURL = await SpotImage.findOne({
@@ -47,16 +149,16 @@ router.get('/', async (req, res) => {
                 spotId: spot.id,
                 preview: true
             },
-            attributes: ['url'], raw: true
+            attributes: ['url']
         });
 
-        spotsArr.push(spot);
-        spot.avgRating = (Number(ratings[0].avgRating) / spotsArr.length).toFixed(1);
+        result.push(spot);
+        spot.avgRating = Number(ratings[0].toJSON().avgRating);
         spot.previewImage = imageURL.url;
     };
 
-    res.status(200).json({ Spots: spotsArr });
-    // res.status(200).json({ Spots: spotsArr, page, size });
+    res.json({ Spots: result });
+    // res.status(200).json({ Spots: result, page, size });
 });
 
 module.exports = router;
