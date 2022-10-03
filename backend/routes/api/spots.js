eb62c2c079a0c01ac8c9f7f5d42b811c1f226702
Kristopher Han <kris@kristopherhan.com>
2022-10-03 00:22:31 -0700

    moved routes

--- backend/routes/api/spots.js
+++ backend/routes/api/spots.js
@@ -264,49 +264,81 @@ router.post('/:spotId/reviews', requireAuth, async (req, res) => {
                   stars: 'Stars must be an integer from 1 to 5',
                 }
             })
-    }
-});
+        }
+    });
 
-// #07: CREATE A SPOT
-router.post('/', requireAuth, async (req, res) => {
-    const user = req.user;
-    const { address, city, state, country, lat, lng, name, description, price } = req.body;
+    // #07: CREATE A SPOT
+    router.post('/', requireAuth, async (req, res) => {
+        const user = req.user;
+        const { address, city, state, country, lat, lng, name, description, price } = req.body;
 
-    try {
-        const newSpot = await Spot.create({
-            ownerId: user.id,
-            address,
-            city,
-            state,
-            country,
-            lat,
-            lng,
-            name,
-            description,
-            price
-        });
+        try {
+            const newSpot = await Spot.create({
+                ownerId: user.id,
+                address,
+                city,
+                state,
+                country,
+                lat,
+                lng,
+                name,
+                description,
+                price
+            });
 
-        res.status(201).json(newSpot);
-    }
+            res.status(201).json(newSpot);
+        }
 
-    catch {
-        res.status(400);
-        res.json({
-            message: 'Validation Error',
-            statusCode: 400,
-            errors: {
-                'address': 'Street address is required',
-                'city': 'City is required',
-                'state': 'State is required',
-                'country': 'Country is required',
-                'lat': 'Latitude is not valid',
-                'lng': 'Longitude is not valid',
-                'name': 'Name must be less than 50 characters',
-                'description': 'Description is required',
-                'price': 'Price per day is required'
-              }
+        catch {
+            res.status(400);
+            res.json({
+                message: 'Validation Error',
+                statusCode: 400,
+                errors: {
+                    'address': 'Street address is required',
+                    'city': 'City is required',
+                    'state': 'State is required',
+                    'country': 'Country is required',
+                    'lat': 'Latitude is not valid',
+                    'lng': 'Longitude is not valid',
+                    'name': 'Name must be less than 50 characters',
+                    'description': 'Description is required',
+                    'price': 'Price per day is required'
+                }
             });
         };
+    });
+
+    // #11: GET ALL SPOTS OWNED BY THE CURRENT USER
+    router.get('/current', requireAuth, handleValidationErrors, async (req, res) => {
+        const ownerId = req.user.id;
+        const spots = await Spot.findAll({
+            where: { ownerId : ownerId }
+        });
+
+        let avgRating;
+        for (let spot of spots) {
+            const countReviews = await Review.count({ where: { spotId : spot.id } });
+            const stars = await Review.sum('stars', { where: { spotId : spot.id } });
+
+            if (!stars) avgRating = 0;
+            else avgRating = ( stars / countReviews ).toFixed(1);
+
+            spot.avgRating = avgRating;
+
+            const imageURL = await SpotImage.findOne({
+                where: { spotId: spot.id },
+                attributes: [ 'id', 'url', 'preview' ]
+            });
+
+            if (imageURL) {
+                spot.previewImage = imageURL.url;
+            } else {
+                spot.previewImage = null;
+            };
+        };
+
+        res.json({ Spots: spot });
     });
 
 // #28 & 29: GET ALL BOOKINGS FOR A SPOT BY ID
@@ -437,37 +469,6 @@ router.get('/:spotId', async (req, res) => {
     return res.json(result);
 });
 
-// #11: GET ALL SPOTS OWNED BY THE CURRENT USER
-router.get('/current', requireAuth, handleValidationErrors, async (req, res) => {
-    const ownerId = req.user.id;
-    const spots = await Spot.findAll({
-        where: { ownerId : ownerId }
-    });
-
-    let avgRating;
-    for (let spot of spots) {
-        const countReviews = await Review.count({ where: { spotId : spot.id } });
-        const stars = await Review.sum('stars', { where: { spotId : spot.id } });
-
-        if (!stars) avgRating = 0;
-        else avgRating = ( stars / countReviews ).toFixed(1);
-
-        spot.avgRating = avgRating;
-
-        const imageURL = await SpotImage.findOne({
-            where: { spotId: spot.id },
-            attributes: [ 'id', 'url', 'preview' ]
-        });
-
-        if (imageURL) {
-            spot.previewImage = imageURL.url;
-        } else {
-            spot.previewImage = null;
-        };
-    };
-
-    res.json({ Spots: spot });
-})
 
 // router.get('/current', requireAuth, async (req, res) => {
 //     const ownerId = req.user.id;
