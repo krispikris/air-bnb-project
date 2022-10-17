f230a90c849b1e38626235cf54fabc60cf852ef3
Kristopher Han <kris@kristopherhan.com>
2022-10-17 02:48:11 -0700

    front deployment | phase: 1 | completed

--- backend/routes/index.js
+++ backend/routes/index.js
@@ -29,14 +29,14 @@ if (process.env.NODE_ENV === 'production') {
   });
 }
 
-// Add a XSRF-TOKEN cookie
-router.get("/api/csrf/restore", (req, res) => {
-  const csrfToken = req.csrfToken();
-  res.cookie("XSRF-TOKEN", csrfToken);
-  res.status(200).json({
-    'XSRF-Token': csrfToken
-  });
-});
+// // Add a XSRF-TOKEN cookie
+// router.get("/api/csrf/restore", (req, res) => {
+//   const csrfToken = req.csrfToken();
+//   res.cookie("XSRF-TOKEN", csrfToken);
+//   res.status(200).json({
+//     'XSRF-Token': csrfToken
+//   });
+// });
 
   // Add a XSRF-TOKEN cookie in development
   if (process.env.NODE_ENV !== 'production') {
