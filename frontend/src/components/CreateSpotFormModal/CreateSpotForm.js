e436862d4b52e243866c6ed230a00c642af0f198
Kristopher Han <kris@kristopherhan.com>
2022-10-19 19:45:52 -0700

    CreateSpotForm | COMPLETED | converted spotForm to Modal | updating Navigation and ModalContext next

---
+++ frontend/src/components/CreateSpotFormModal/CreateSpotForm.js
@@ -0,0 +1,136 @@
+import    React, { useState }       from "react";
+import  { useDispatch }             from "react-redux";
+import  { useHistory, useParams }   from "react-router-dom";
+import  { createSpotThunk }         from "../../store/spots";
+import                                   "./CreateSpotForm.css";
+
+const CreateSpotForm = () => {
+    const history = useHistory();
+    const dispatch = useDispatch();
+
+    const [address, setAddress]             = useState('');
+    const [city, setCity]                   = useState('');
+    const [state, setState]                 = useState('');
+    const [country, setCountry]             = useState('');
+    const [name, setName]                   = useState('');
+    const [description, setDescription]     = useState('');
+    const [price, setPrice]                 = useState('');
+    const [imageURL, setimageURL]           = useState('');
+
+    const handleSubmit = async (e) => {
+        e.preventDefault();
+
+        let spotFormInputs = {
+                address,
+                city,
+                state,
+                country,
+                name,
+                description,
+                price
+        };
+
+        const newSpot = await dispatch(createSpotThunk(spotFormInputs));
+        if (newSpot) {
+            const img = ({
+                url: imageURL,
+                preview: true
+            })
+            // await dispatch(createSpotImageThunk(newSpot.id, img));
+            return history.push(`/spots/${newSpot.id}`);
+        };
+    };
+
+return (
+    <form
+        className='create-new-spot-form'
+        onSubmit={handleSubmit}
+        >
+
+        <label>
+            Address
+            <input
+            type="text"
+            name="address"
+            value={address}
+            onChange={e => setAddress(e.target.value)}
+            />
+        </label>
+
+        <label>
+            City
+            <input
+            type="text"
+            name="city"
+            value={city}
+            onChange={e => setCity(e.target.value)}
+            />
+        </label>
+
+        <label>
+            State
+            <input
+            type="text"
+            name="state"
+            value={state}
+            onChange={e => setState(e.target.value)}
+            />
+        </label>
+
+        <label>
+            Country
+            <input
+            type="text"
+            name="country"
+            value={country}
+            onChange={e => setCountry(e.target.value)}
+            />
+        </label>
+
+        <label>
+            Name
+            <input
+            type="text"
+            name="name"
+            value={name}
+            onChange={e => setName(e.target.value)}
+            />
+        </label>
+
+        <label>
+            Description
+            <input
+            type="text"
+            name="description"
+            value={description}
+            onChange={e => setDescription(e.target.value)}
+            />
+        </label>
+
+        <label>
+            Price
+            <input
+            type="text"
+            name="price"
+            value={price}
+            onChange={e => setPrice(e.target.value)}
+            />
+        </label>
+
+        <label>
+            Upload Image URL
+            <input
+            type="text"
+            name="imageURL"
+            value={imageURL}
+            onChange={e => setimageURL(e.target.value)}
+            />
+        </label>
+
+        <button type="submit">Create New Spot</button>
+
+        </form>
+    )
+}
+
+export default CreateSpotForm;
---
+++ frontend/src/components/CreateSpotFormModal/index.js
@@ -0,0 +1,21 @@
+// frontend/src/components/CreateSpotModal/index.js
+import    React, { useState }   from 'react';
+import  { Modal }               from '../../context/Modal';
+import    CreateSpotForm        from './CreateSpotForm';
+
+const CreateSpotFormModal = () => {
+    const [showModal, setShowModal] = useState(false);
+
+    return (
+      <>
+        <button  onClick={() => setShowModal(true)}>Create New Spot</button>
+        {showModal && (
+          <Modal onClose={() => setShowModal(false)}>
+            <CreateSpotForm />
+          </Modal>
+        )}
+      </>
+    );
+  }
+
+  export default CreateSpotFormModal;
